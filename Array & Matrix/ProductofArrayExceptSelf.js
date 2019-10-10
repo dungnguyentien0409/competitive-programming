6c408d6667a7e012de47d1e7a6ec8a95ff514d09
DungNguyen <nguyentien.dung@petronas.com.my>
2019-10-10 17:24:52 +0800

    Solution for ProductofArrayExceptSelf

---
+++ Array & Matrix/ProductofArrayExceptSelf.js
@@ -0,0 +1,28 @@
+/**
+ * Problem: https://leetcode.com/problems/product-of-array-except-self/submissions/
+ * Author: Dung Nguyen Tien (Chris)
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    if (nums.length < 2) return [];
+    
+    var n = nums.length;
+    var res = Array(n).fill(1);
+    
+    // product left value except self
+    var leftValue = 1;
+    for (var i = 1; i < n; i++) {
+        leftValue *= nums[i - 1];
+        res[i] = res[i] * leftValue;
+    }
+
+    // product with product of right value except sefl
+    var rightValue = 1;
+    for (var i = n - 2; i >= 0; i--) {
+        rightValue *= nums[i + 1];
+        res[i] *= rightValue;
+    }
+    
+    return res;
+};
