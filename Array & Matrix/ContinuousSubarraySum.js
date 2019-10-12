a024664ca9ee24d0fcca38a2f2840199fcd3e118
DungNguyen <nguyentien.dung@petronas.com.my>
2019-10-12 17:38:46 +0800

    Solution for ContinuousSubarraySum and LongestConsecutiveSequence

---
+++ Array & Matrix/ContinuousSubarraySum.js
@@ -0,0 +1,29 @@
+/**
+ * Problem: https://leetcode.com/problems/continuous-subarray-sum/submissions/
+ * Author: Dung Nguyen Tien (Chris)
+ * Implement following idea of compton_scatter
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    var map = {};
+    var sum = 0;
+    
+    map[0] = -1;
+    for (var i = 0; i < nums.length; i++) {
+        sum = sum + nums[i];
+        
+        if (k != 0) sum = sum % k;
+        // when we meet sum again, mean that sum was added a number of multiple of k. so we substract the current i with the previous index to count the length: (a + n * k) % k == a % k
+        if (map[sum] != undefined) {
+            if (i - map[sum] > 1)
+                return true;
+        }
+        else {
+            map[sum] = i;
+        }
+    }
+    
+    return false;
+};
---
+++ Array & Matrix/LongestConsecutiveSequence.js
@@ -0,0 +1,28 @@
+/**
+ * Problem: https://leetcode.com/problems/longest-consecutive-sequence/submissions/
+ * Author: Dung Nguyen Tien (Chris)
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) { 
+    var longest = 0;
+    var map = {};
+    
+    // each boundaries n: have the value of the longest string inside
+    for (var n of nums) {
+        if (map[n] == undefined) {
+            var left = map[n - 1] || 0; // length of boundary n - 1
+            var right = map[n + 1] || 0; // length of boundary n + 1
+            var length = left + right + 1;
+            
+            map[n] = length;
+            longest = Math.max(longest, length);
+            
+            // update the left boundary of left and right boundary of right
+            map[n - left] = length;
+            map[n + right] = length;
+        }
+    }
+    
+    return longest;
+};
