8d7898f2dc90ad302152c516214d7f5f94e35e63
DungNguyen <nguyentien.dung@petronas.com.my>
2019-10-17 15:03:55 +0800

    Solution for Subset2

---
+++ BackTracking/Subset2.js
@@ -0,0 +1,27 @@
+/**
+ * Problem: https://leetcode.com/problems/subsets-ii/
+ * Author: Dung Nguyen Tien (Chris)
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var subsetsWithDup = function(nums) {
+    nums.sort(function(a, b) {return a - b;});
+    
+    var res = [];
+    backTrack(res, [], nums, 0);
+    
+    return res;
+};
+
+function backTrack(res, tmp, nums, start) {
+    res.push(tmp.slice(0));
+    
+    for (var i = start; i < nums.length; i++) {
+        // skip the duplicated item
+        if (i > start && nums[i] == nums[i - 1]) continue;
+        
+        tmp.push(nums[i]);
+        backTrack(res, tmp, nums, i + 1);
+        tmp.pop();
+    }
+}
