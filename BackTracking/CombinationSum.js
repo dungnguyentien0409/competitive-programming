42a4e1be484c2dbe12cdc20a5e7bbd5870403af6
DungNguyen <nguyentien.dung@petronas.com.my>
2019-10-17 14:38:45 +0800

    Solution for CombinationSum

---
+++ BackTracking/CombinationSum.js
@@ -0,0 +1,29 @@
+/**
+ * Problem: https://leetcode.com/problems/combination-sum/
+ * Author: Dung Nguyen Tien (Chris)
+ * @param {number[]} candidates
+ * @param {number} target
+ * @return {number[][]}
+ */
+var combinationSum = function(candidates, target) {
+    candidates.sort(function(a,b) {return a - b;})
+    
+    var res = [];
+    backTrack(res, [], candidates, target, 0);
+    
+    return res;
+};
+
+function backTrack(res, tmp, nums, target, start) {
+    if (target < 0) return;
+    else if (target == 0) res.push(tmp.slice(0));
+    else {
+        for (var i = start; i < nums.length; i++) {
+            // pick one solution
+            tmp.push(nums[i]);
+            //back track
+            backTrack(res, tmp, nums, target - nums[i], i);
+            tmp.pop();
+        }
+    }
+}
