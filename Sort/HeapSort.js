971a06ebf59d5a791d7c6e0aad5289ac7dd1d6ee
DungNguyen <nguyentien.dung@petronas.com.my>
2019-10-23 10:32:39 +0800

    edit heapsort

--- Sort/HeapSort.js
+++ Sort/HeapSort.js
@@ -3,39 +3,39 @@
  * Author: Dung Nguyen Tien (Chris)
 */
 
-function heapify(arr, i) {
+function heapify(arr, n, i) {
   var left = 2 * i;
   var right = 2 * i + 1;
   var imax = i;
   
-  if (left < arr.length && arr[left] > arr[imax]) imax = left;
-  if (right < arr.length && arr[right] > arr[imax]) imax = right;
+  if (left < n && arr[left] > arr[imax]) imax = left;
+  if (right < n && arr[right] > arr[imax]) imax = right;
   
   if (imax != i) {
     if (imax == left) {
       [arr[i], arr[left]] = [arr[left], arr[i]];
-      heapify(arr, left);
+      heapify(arr, n, left);
     }
     else {
       [arr[i], arr[right]] = [arr[right], arr[i]];
-      heapify(arr, right);
+      heapify(arr, n, right);
     }
   }
 }
 
-function buildHeap(arr, index) {
-  var n = Math.floor(index / 2);
+function buildHeap(arr) {
+  var n = Math.floor(arr.length / 2) - 1;
   
   for (var i = n; i >= 0; i--) {
-    heapify(arr, i);
+    heapify(arr, arr.length, i);
   }
 }
 
 function heapSort(arr) {
+  buildHeap(arr);
   for (var i = arr.length - 1; i >= 0; i--) {
-    buildHeap(arr, i);
-    document.writeln(arr[0]);
     [arr[0], arr[i]] = [arr[i], arr[0]];
+    heapify(arr, i, 0);
   }
 }
 
