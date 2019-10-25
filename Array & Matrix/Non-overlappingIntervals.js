16f36d50eac581b2ebe6acca162783d1ac9395ed
DungNguyen <nguyentien.dung@petronas.com.my>
2019-10-25 18:03:27 +0800

    Solution for Non-overlappingIntervals

---
+++ Array & Matrix/Non-overlappingIntervals.js
@@ -0,0 +1,28 @@
+/**
+ * Problem: https://leetcode.com/problems/non-overlapping-intervals/
+ * Author: Dung Nguyen Tien (Chris)
+ * @param {number[][]} intervals
+ * @return {number}
+ */
+var eraseOverlapIntervals = function(intervals) {
+    if (intervals.length == 0) return 0;
+    
+    intervals.sort(function(a, b) {return a[1] - b[1]; });
+    
+    var remove = 0;
+    var range = intervals[0];
+    for (var i = 1; i < intervals.length; i++) {
+        var interval = intervals[i];
+        
+        if (range[1] > interval[0]) {
+            // if overlapping then remove
+            remove++;
+        }
+        else {
+            // if not overlapping, update the currently considered range
+            range = interval.slice(0);
+        }
+    }
+    
+    return remove;
+};
