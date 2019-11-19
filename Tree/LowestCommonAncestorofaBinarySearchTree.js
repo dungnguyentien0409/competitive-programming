b903d5e48b7306d84d3c8d59674dec70351115ad
DungNguyen <nguyentien.dung@petronas.com.my>
2019-11-20 00:42:14 +0800

    Solution for LowestCommonAncestorofaBinarySearchTree in js

---
+++ Tree/LowestCommonAncestorofaBinarySearchTree.js
@@ -0,0 +1,25 @@
+/**
+ * Problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/submissions/
+ * Author: Dung Nguyen Tien (Chris)
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {TreeNode} p
+ * @param {TreeNode} q
+ * @return {TreeNode}
+ */
+var lowestCommonAncestor = function(root, p, q) {
+    if (root == null || root.val == p.val || root.val == q.val) return root;
+    
+    var left = lowestCommonAncestor(root.left, p, q);
+    var right = lowestCommonAncestor(root.right, p, q);
+    
+    if (left != null && right != null) return root;
+    
+    return left != null ? left : right;
+};
