b4ad2377e54372f5ce3d03f69001a24b22e1d241
DungNguyen <nguyentien.dung@petronas.com.my>
2019-11-20 00:22:48 +0800

    Solution for LowestCommonAncestorofaBinarySearchTree.cs

---
+++ Tree/LowestCommonAncestorofaBinarySearchTree.cs
@@ -0,0 +1,23 @@
+/**
+ * Problem: https://leetcode.com/problems/lowest-common-ancestor-of-a-binary-search-tree/
+ * Author: Dung Nguyen Tien (Chris)
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode LowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null || root.val == p.val || root.val == q.val) return root;
+        
+        var left = LowestCommonAncestor(root.left, p, q);
+        var right = LowestCommonAncestor(root.right, p, q);
+        
+        if (left != null && right != null) return root;
+        
+        return left != null ? left : right;
+    }
+}
