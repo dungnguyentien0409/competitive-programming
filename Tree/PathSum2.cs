495f51e3a28eb85b4b1e1bde8663c7085d4d4a22
DungNguyen <nguyentien.dung@petronas.com.my>
2019-11-21 00:55:36 +0800

    Solution for PathSum2 in cs

---
+++ Tree/PathSum2.cs
@@ -0,0 +1,36 @@
+/**
+ * Problem: https://leetcode.com/problems/path-sum-ii/submissions/
+ * Author: Dung Nguyen Tien (Chris)
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> PathSum(TreeNode root, int sum) {
+        IList<IList<int>> res = new List<IList<int>>();
+        
+        GoSum(root, 0, sum, new List<int>(), res);
+        return res;
+    }
+    
+    public void GoSum(TreeNode node, int curS, int sum, List<int> curRes, IList<IList<int>> res) {
+        if (node == null) return;
+        
+        curS += node.val;
+        curRes.Add(node.val);
+        if (curS == sum && IsLeaf(node)) res.Add(new List<int>(curRes));
+        
+        GoSum(node.left, curS, sum, curRes, res);
+        GoSum(node.right, curS, sum, curRes, res);
+        
+        curRes.RemoveAt(curRes.Count - 1);
+    }
+    
+    public bool IsLeaf(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+}
