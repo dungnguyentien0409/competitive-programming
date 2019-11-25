66fccc60a5da39a2945cfe46aee8bcf42cb511a7
DungNguyen <nguyentien.dung@petronas.com.my>
2019-11-25 15:41:30 +0800

    Solution for NumberOfIslands in js

---
+++ Recursion/NumberOfIslands.js
@@ -0,0 +1,35 @@
+/**
+ * Problem: https://leetcode.com/problems/number-of-islands/
+ * Author: Dung Nguyen Tien (Chris)
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    if (grid.length == 0 || grid[0].length == 0) return 0;
+    
+    var rows = grid.length;
+    var cols = grid[0].length;
+    var res = 0;
+    
+    for (var i = 0; i < rows; i++) {
+        for (var j = 0; j < cols; j++) {
+            res += mark(grid, i, j);
+        }
+    }
+    
+    return res;
+};
+
+function mark(grid, i, j) {
+    if (i < 0 || i > grid.length - 1 || j < 0 || j > grid[0].length - 1 || grid[i][j] == 0) {
+        return 0;
+    }
+    
+    grid[i][j] = 0;
+    mark(grid, i - 1, j);
+    mark(grid, i + 1, j);
+    mark(grid, i, j + 1);
+    mark(grid, i, j - 1);
+    
+    return 1;
+}
