3c126b28fc5bce5f1968c3ca3b9d175adff1d08f
DungNguyen <nguyentien.dung@petronas.com.my>
2019-11-28 15:24:35 +0800

    Solution for ConstructBinarySearchTreeFromPreorderTraversal

---
+++ Tree/ConstructBinarySearchTreeFromPreorderTraversal.js
@@ -0,0 +1,30 @@
+/**
+ * Problem: https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+ * Author: Dung Nguyen Tien (Chris)
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {number[]} preorder
+ * @return {TreeNode}
+ */
+var bstFromPreorder = function(preorder) {
+    var root = helper(preorder, Number.MAX_SAFE_INT, Number.MIN_SAFE_INT);
+    
+    return root;
+};
+
+function helper(preorder, max, min) {
+    if (preorder.length == 0) return null;
+    
+    if (preorder[0] >= max || preorder[0] <= min) return null;
+    
+    var node = new TreeNode(preorder.shift());
+    node.left = helper(preorder, node.val, min);
+    node.right = helper(preorder, max, node.val);
+    
+    return node;
+}
