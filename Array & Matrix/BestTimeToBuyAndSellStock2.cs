a1199ece6ba61171458b5822ff95dd300df379fd
DungNguyen <nguyentien.dung@petronas.com.my>
2019-11-29 10:51:02 +0800

    Solution for BestTimeToBuyAndSellStock2

---
+++ Array & Matrix/BestTimeToBuyAndSellStock2.cs
@@ -0,0 +1,18 @@
+/*
+ * Problem: https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/
+ * Author: Dung Nguyen Tien (Chris)
+*/
+
+public class Solution {
+    public int MaxProfit(int[] prices) {
+        var buy = Int32.MinValue;
+        var sell = 0;
+        
+        for (var i = 0; i < prices.Length; i++) {
+            buy = Math.Max(buy, sell - prices[i]);
+            sell = Math.Max(sell, buy + prices[i]);
+        }
+        
+        return sell;
+    }
+}
