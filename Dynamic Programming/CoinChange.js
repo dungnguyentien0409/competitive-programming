3eedfc9668422162d6b7d42024acbc3d14ea726c
DungNguyen <nguyentien.dung@petronas.com.my>
2019-12-08 18:24:55 +0800

    Update coin change solution

--- Dynamic Programming/CoinChange.js
+++ Dynamic Programming/CoinChange.js
@@ -6,18 +6,15 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    // dp with i: the current total money, dp[i]: min coin to get that money
     var dp = Array(amount + 1).fill(Number.MAX_SAFE_INTEGER);
     dp[0] = 0;
     
-    for (var i = 1; i <= amount; i++) {
-        for (var j = 0; j < coins.length; j++) {
-            if (i >= coins[j]) {
-                // with i: total money, check all the previous value of coin. let say dp[i - 1] + 1 = dp[i] for coin 1, dp[i - 2] + 1 for coin 2, and get the min
-                dp[i] = Math.min(dp[i], dp[i - coins[j]] + 1);
-            }
+    // put each coin into the value
+    for (var c of coins) {
+        for (var i = c; i <= amount; i++) {
+            dp[i] = Math.min(dp[i], dp[i - c] + 1);
         }
     }
-    // return the min coin dp[amount] for the amout: i = amount
-    return dp[amount] != Number.MAX_SAFE_INTEGER ? dp[amount] : -1;
+    
+    return dp[amount] == Number.MAX_SAFE_INTEGER ? -1 : dp[amount];
 };
