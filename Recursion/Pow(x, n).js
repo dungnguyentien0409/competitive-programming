9a43137f6224f2aaa04e3e627fd81e72d170b4b0
DungNguyen <nguyentien.dung@petronas.com.my>
2019-12-09 18:17:54 +0800

    Solution for Pow(x, n)

---
+++ Recursion/Pow(x, n).js
@@ -0,0 +1,17 @@
+/**
+ * Problem: https://leetcode.com/problems/powx-n/
+ * Author: Dung Nguyen Tien (Chris)
+ * @param {number} x
+ * @param {number} n
+ * @return {number}
+ */
+var myPow = function(x, n) {
+    if(n == 0) return 1;
+    if(n < 0){
+        n = -n;
+        x = 1 / x;
+    }
+    
+    return (n % 2 == 0) ? 
+            myPow(x * x, Math.floor(n / 2)) : x * myPow(x * x, Math.floor(n / 2));
+};
