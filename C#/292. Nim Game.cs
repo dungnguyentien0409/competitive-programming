06380bdd52056916aea922cc34c400b24a5fa239
DungNguyen <nguyentien.dung@petronas.com.my>
2020-04-02 15:04:19 +0800

    Solution for Nim Game

---
+++ C#/292. Nim Game.cs
@@ -0,0 +1,10 @@
+/*
+ * Link: https://leetcode.com/problems/nim-game/
+ * Author: Dung Nguyen Tien (Chris)
+*/
+
+public class Solution {
+    public bool CanWinNim(int n) {
+        return n % 4 != 0;
+    }
+}
