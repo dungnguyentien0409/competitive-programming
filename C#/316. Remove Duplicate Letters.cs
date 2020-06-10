f3286aec271e72ef686584a4cc9f434f41c64a30
DungNguyen <dungnguyentien0409@gmail.com>
2020-06-10 15:53:25 +0800

    Solution for Remove Duplicate Letters

---
+++ C#/316. Remove Duplicate Letters.cs
@@ -0,0 +1,38 @@
+/*
+ * Link: https://leetcode.com/problems/remove-duplicate-letters/submissions/
+ * Author: Dung Nguyen Tien (Chris)
+*/
+
+public class Solution {
+    public string RemoveDuplicateLetters(string s) {
+        var map = new Dictionary<char, int>();
+        var IsInStack = new Dictionary<char, bool>();
+        var st = new Stack<char>();
+        
+        foreach(var c in s) {
+            if (!map.ContainsKey(c)) {
+                map.Add(c, 0);
+                IsInStack.Add(c, false);
+            }
+            
+            map[c]++;
+        }
+        
+        foreach(var c in s) {
+            map[c]--;
+            if (IsInStack[c]) continue;
+            
+            while (st.Count > 0 && c < st.Peek() && map[st.Peek()] > 0) {
+                IsInStack[st.Pop()] = false;
+            }
+            
+            st.Push(c);
+            IsInStack[c] = true;
+        }
+        
+        var arr = st.ToArray();
+        Array.Reverse(arr);
+        var res = string.Join("", arr);
+        return res;
+    }
+}
