066107bc5fbe53f8ec5680a6e9c288331b8cd07c
DungNguyen <dungnguyentien0409@gmail.com>
2020-06-17 15:07:55 +0800

    Solution for Rotated Digits

---
+++ C#/788. Rotated Digits.cs
@@ -0,0 +1,31 @@
+/*
+ * Link: https://leetcode.com/problems/rotated-digits/
+ * Author: Dung Nguyen Tien (Chris)
+*/
+
+public class Solution {
+    public int RotatedDigits(int N) {
+        var dp = new int[N + 1];
+        var count = 0;
+        
+        for(var i = 0; i <= N; i++) {
+            if (i < 10) {
+                if (i == 0 || i == 1 || i == 8) dp[i] = 1;
+                else if (i == 2 || i == 5 || i == 6 || i == 9) {
+                    dp[i] = 2;
+                    count++;
+                }
+            }
+            else {
+                int a = dp[i / 10], b = dp[i % 10];
+                if (a == 1 && b == 1) dp[i] = 1;
+                else if (a >= 1 && b >= 1) {
+                    dp[i] = 2;
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+}
