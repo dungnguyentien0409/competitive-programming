45d16653559635a653ead90b709f4b1b87f27779
DungNguyen <dungnguyentien0409@gmail.com>
2020-07-02 15:14:44 +0800

    Solution for Binary Tree Level Order Traversal

---
+++ C#/102. Binary Tree Level Order Traversal.cs
@@ -0,0 +1,41 @@
+/**
+ * Link: https://leetcode.com/problems/binary-tree-level-order-traversal/
+ * Author: Dung Nguyen Tien (Chris)
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+        if (root == null) return new List<IList<int>>();
+        
+        var res = new List<IList<int>>();
+        var queue = new Queue<TreeNode>();
+        
+        queue.Enqueue(root);
+        while(queue.Count > 0) {
+            var len = queue.Count;
+            var current = new List<int>();
+            
+            for(var i = 0; i < len; i++) {
+                var node = queue.Dequeue();
+                
+                current.Add(node.val);
+                if (node.left != null) queue.Enqueue(node.left);
+                if (node.right != null) queue.Enqueue(node.right);
+            }
+            
+            res.Add(current);
+        }
+        
+        return res;
+    }
+}
