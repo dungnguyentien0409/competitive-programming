d10d0f511a48012f42736cfc000f9370a94ee0ce
DungNguyen <dungnguyentien0409@gmail.com>
2020-07-03 17:12:18 +0800

    Update solution for Majority element 2

--- C#/229. Majority Element II.cs
+++ C#/229. Majority Element II.cs
@@ -8,33 +8,23 @@ public class Solution {
         int n1 = 0, n2 = 0, c1 = 0, c2 = 0;
         
         foreach(var n in nums) {
-            if (c1 > 0 && c2 > 0) {
-                if (n != n1 && n != n2) {
-                    c1--;
-                    c2--;
-                }
-                else {
-                    c1 = (n == n1) ? c1++ : c1;
-                    c2 = (n == n2) ? c2++ : c2;
-                }
+            if (n == n1) {
+                c1++;
             }
-            else if (c1 > 0) {
-                if (n == n1) c1++;
-                else {
-                    n2 = n;
-                    c2++;
-                }
+            else if (n == n2) {
+                c2++;
             }
-            else if (c2 > 0) {
-                if (n == n2) c2++;
-                else {
-                    n1 = n;
-                    c1++;
-                }
+            else if (c1 == 0) {
+                n1 = n; 
+                c1++;
+            }
+            else if (c2 == 0) {
+                n2 = n;
+                c2++;
             }
             else {
-                n1 = n;
-                c1++;
+                c1--;
+                c2--;
             }
         }
         
