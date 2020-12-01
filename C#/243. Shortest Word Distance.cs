cdcee6fecc523a9e29f0b1c3c2d56601f772475d
DungNguyen <dungnguyentien0409@gmail.com>
2020-12-01 17:11:17 +0800

    update solution for shortest word distance

--- C#/243. Shortest Word Distance.cs
+++ C#/243. Shortest Word Distance.cs
@@ -1,32 +1,26 @@
 /*
- * Link: https://leetcode.com/problems/shortest-word-distance/submissions/
+ * Link: https://leetcode.com/problems/shortest-word-distance/
  * Author: Dung Nguyen Tien (Chris)
 */
 
 public class Solution {
     public int ShortestDistance(string[] words, string word1, string word2) {
-        int i1 = -1, i2 = -1;
-        var shortest = words.Length;
+        int i1 = -words.Length, i2 = words.Length;
+        int d = words.Length;
         
-        for(var i = 0; i < words.Length; i++) {
-            var word = words[i];
+        for (var i = 0; i < words.Length; i++) {
+            var w = words[i];
             
-            if (word.CompareTo(word1) == 0) {
+            if (w.CompareTo(word1) == 0) {
                 i1 = i;
-                
-                if (i2 > -1) {
-                    shortest = Math.Min(shortest, Math.Abs(i1 - i2));
-                }
             }
-            else if (word.CompareTo(word2) == 0) {
+            else if(w.CompareTo(word2) == 0) {
                 i2 = i;
-                
-                if (i1 > -1) {
-                    shortest = Math.Min(shortest, Math.Abs(i1 - i2));
-                }
             }
+            
+            d = Math.Min(d, Math.Abs(i1 - i2));
         }
         
-        return shortest;
+        return d;
     }
 }
