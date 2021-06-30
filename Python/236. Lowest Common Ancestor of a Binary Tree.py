faf8c180189294ae682a817bda60dff8eebb93d7
Nguyen Tien Dung <nguyentien.dung@petronas.com>
2021-06-30 23:00:42 +0800

    solution for lowest common ancestor of a BT

---
+++ Python/236. Lowest Common Ancestor of a Binary Tree.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def lowestCommonAncestor(self, root, p, q):
+        """
+        :type root: TreeNode
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: TreeNode
+        """
+        if root == None or root.val == p. val or root.val == q.val:
+            return root
+        
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        x = left.val if left != None else 0
+        y = right.val if right != None else 0
+        print("{} {}".format(x, y))
+        
+        if left != None and right != None:
+            return root
+        if left != None: return left; 
+        return right
